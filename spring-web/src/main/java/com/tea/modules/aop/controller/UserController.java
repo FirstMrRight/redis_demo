be65080cc8bde7d44a97e717407d435b6f0f719a
jaymin <951526273@qq.com>
2021-05-27 13:53:24 +0800

    注入线程池

--- spring-web/src/main/java/com/tea/modules/aop/controller/UserController.java
+++ spring-web/src/main/java/com/tea/modules/aop/controller/UserController.java
@@ -3,10 +3,14 @@ package com.tea.modules.aop.controller;
 import com.tea.modules.model.po.User;
 import com.tea.modules.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
 
 /**
  * @author jaymin
@@ -18,6 +22,9 @@ public class UserController {
 
     @Autowired
     private UserService userService;
+
+    @Resource(name = "taskExecutor")
+    private ThreadPoolTaskExecutor threadPool;
 
     @PostMapping("/login")
     public User login(@RequestBody User user){
