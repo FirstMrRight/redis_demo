6e99e8bfec4ebb24509b7705b4ec596a0413cb36
jaymin <951526273@qq.com>
2021-06-04 14:41:56 +0800

    反射获取父类方法并调用

--- spring-core/src/main/java/com/tea/modules/java8/reflect/MethodReflector.java
+++ spring-core/src/main/java/com/tea/modules/java8/reflect/MethodReflector.java
@@ -1,6 +1,7 @@
 package com.tea.modules.java8.reflect;
 
 import com.tea.modules.model.po.ReflectBean;
+import com.tea.modules.model.po.User;
 
 import java.lang.reflect.Constructor;
 import java.lang.reflect.InvocationTargetException;
@@ -15,6 +16,11 @@ import java.lang.reflect.Method;
 @SuppressWarnings("all")
 public class MethodReflector {
     public static void main(String[] args) throws ClassNotFoundException, NoSuchMethodException, IllegalAccessException, InvocationTargetException, InstantiationException {
+        invokeMethod();
+        superMethodNotFound();
+    }
+
+    private static void invokeMethod() throws ClassNotFoundException, NoSuchMethodException, InstantiationException, IllegalAccessException, InvocationTargetException {
         // 1. 获取Class对象
         Class clazz = Class.forName("com.tea.modules.model.po.ReflectBean");
         // 2. 获取所有公有方法,获取到的methods是可以获取到父类的方法的，包括Object
@@ -45,5 +51,16 @@ public class MethodReflector {
         String invokeResult = String.valueOf(getMessage.invoke(reflectBean, 1));
         System.out.println(invokeResult);
         display.invoke(reflectBean, "Display");
+    }
+
+    /**
+     * <h2>调用父类的方法</h2>
+     */
+    private static void superMethodNotFound() throws NoSuchMethodException, InvocationTargetException, IllegalAccessException {
+        Class<User> userClass = User.class;
+/*        Method initialize = userClass.getDeclaredMethod("initialize", null);
+        Object invoke = initialize.invoke(null);*/
+        Method superMethod = userClass.getSuperclass().getDeclaredMethod("initialize", String.class);
+        superMethod.invoke(new User(),"id");
     }
 }
